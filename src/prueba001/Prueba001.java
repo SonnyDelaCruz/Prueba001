7dee15a990037f79a1d13b3f40a282147b653eb7
SonnyDelaCruz <gabriel.delacruza@unmsm.edu.pe>
2024-09-09 10:01:46 -0500

    primer commit

---
+++ src/prueba001/Prueba001.java
@@ -0,0 +1,18 @@
+
+package prueba001;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Prueba001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Java");
+    }
+    
+}
